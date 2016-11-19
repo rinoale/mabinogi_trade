6303788a26d63aad40419746721d7c5133821a11
rinoale <rinoale@hanmail.net>
2016-11-19 10:01:12 +0000

    join page added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :username, :emailid, :password_digest, presence: { message: 'can not be empty' }
+  validates :username, :emailid, uniqueness: { message: 'already exists' }
 end
