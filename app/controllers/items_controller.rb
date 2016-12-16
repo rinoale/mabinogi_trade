9da959ca1a485d1a9dd599353d2d7abd164b6aa9
rinoale <rinoale@hanmail.net>
2016-12-16 07:21:54 +0000

    show action and view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,13 @@
 class ItemsController < ApplicationController
+  before_filter :require_login
+
   def show
     @items = current_user.items
+  end
+
+  private
+
+  def require_login
+    redirect_to :root if current_user.nil?
   end
 end
