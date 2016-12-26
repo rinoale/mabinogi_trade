eb87a169fdb4caa4b0f61fadc3b42a702bc62f32
rinoale <rinoale@hanmail.net>
2016-12-26 08:45:09 +0000

    new item forms. creating based on db relation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,29 @@
 class ItemsController < ApplicationController
   before_filter :require_login
 
+  def new
+    @item = current_user.items.new
+  end
+
   def show
     @items = current_user.items
   end
 
+  def create
+    @item = current_user.items.new(item_params)
+
+    if @item.save
+      redirect_to '/items'
+    else
+      render 'new'
+    end
+  end
+
   private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :race, :cloth, :gender, :metalware_info, :inchant_info, :description)
+  end
 
   def require_login
     redirect_to :root if current_user.nil?
