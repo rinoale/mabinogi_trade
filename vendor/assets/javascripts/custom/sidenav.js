67d0363b6d581f048caff03428229ba9f14c97c8
rinoale <rinoale@hanmail.net>
2017-01-13 09:11:04 +0000

    nav push style.

--- vendor/assets/javascripts/custom/sidenav.js
+++ vendor/assets/javascripts/custom/sidenav.js
@@ -5,3 +5,13 @@ function openNav() {
 function closeNav() {
   document.getElementById('sideMenu').style.width = '0';
 }
+
+function openNavPush() {
+  document.getElementById('sideMenu').style.width = '250px';
+  document.getElementById('main').style.marginLeft = '250px';
+}
+
+function closeNavPush() {
+  document.getElementById('sideMenu').style.width = '0';
+  document.getElementById('main').style.marginLeft = '0';
+}
